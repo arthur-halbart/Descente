88f64b5abfc8b863b9c53c46dc04aff94240ea27
Arthur Halbart <arthur.halbart@gmail.com>
2026-08-20 17:10:01 +0200

    Build multi page

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -5,10 +6,22 @@ export default defineConfig({
     root: "src",
     publicDir: "../assets",
     build: {
-    outDir: "../dist",
-    emptyOutDir: true,
+        outDir: "../dist",
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "src/index.html"),
+                avarice: resolve(__dirname, "src/avarice.html"),
+                colere: resolve(__dirname, "src/colere.html"),
+                envie: resolve(__dirname, "src/envie.html"),
+                gourmandise: resolve(__dirname, "src/gourmandise.html"),
+                luxure: resolve(__dirname, "src/luxure.html"),
+                orgueil: resolve(__dirname, "src/orgueil.html"),
+                paresse: resolve(__dirname, "src/paresse.html"),
+            },
+        },
     },
     server: {
-    open: true,
+        open: true,
     },
 });
